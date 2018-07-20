ffb95b53a3314773f6af12a10d3ff0e615adc0fb
Carlos Feria <carlosthe19916@gmail.com>
2018-07-20 16:44:59 -0500

    feat(add support for batches)

---
+++ src/main/resources/db.migration/postgresql/V1_0__initial_script.sql
@@ -0,0 +1,21 @@
+create sequence hibernate_sequence start 1 increment 1;
+
+    create table company (
+       id int8 not null,
+        codigoZona varchar(255),
+        condicionDomicilio varchar(255),
+        departamento varchar(255),
+        estadoContribuyente varchar(255),
+        interior varchar(255),
+        kilometro varchar(255),
+        lote varchar(255),
+        manzana varchar(255),
+        nombreVia varchar(255),
+        numero varchar(255),
+        razonSocial varchar(255),
+        ruc varchar(255),
+        tipoVia varchar(255),
+        tipoZona varchar(255),
+        ubigeo varchar(255),
+        primary key (id)
+    );
