af4d7e93c04ffd19d91c5f8963654eb3ec7f6f24
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-16 13:00:21 +0100

    Change native tests

--- src/test/java/io/github/project/openubl/searchpe/resources/NativeContribuyenteResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.github.project.openubl.searchpe.resources;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeContribuyenteResourceIT extends ContribuyenteResourceTest {
-
-    // Execute the same tests but in native mode.
-}
---
+++ src/test/java/io/github/project/openubl/searchpe/resources/NativeResourceIT.java
@@ -0,0 +1,70 @@
+package io.github.project.openubl.searchpe.resources;
+
+import io.github.project.openubl.searchpe.models.jpa.entity.Status;
+import io.github.project.openubl.searchpe.models.jpa.entity.VersionEntity;
+import io.quarkus.test.junit.NativeImageTest;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@NativeImageTest
+public class NativeResourceIT {
+
+    @Test
+    @Order(1)
+    public void importData() {
+        // Given
+
+        // When
+        VersionEntity versionCreated = given()
+                .header("Content-Type", "application/json")
+                .when()
+                .post("/versions")
+                .then()
+                .statusCode(200)
+                .body(notNullValue())
+                .extract()
+                .as(VersionEntity.class);
+
+        // Then
+        await()
+                .atMost(10, TimeUnit.MINUTES)
+                .until(() -> {
+
+                    VersionEntity versionWatched = given()
+                            .header("Content-Type", "application/json")
+                            .when()
+                            .get("/versions/" + versionCreated.id)
+                            .then()
+                            .extract()
+                            .as(VersionEntity.class);
+
+                    return versionWatched.status == Status.COMPLETED;
+                });
+    }
+
+    @Test
+    @Order(2)
+    public void verifyContribuyentes() {
+        given()
+                .header("Content-Type", "application/json")
+                .when()
+                .get("/contribuyentes")
+                .then()
+                .statusCode(200)
+                .body(
+                        "meta.offset", is(0),
+                        "meta.limit", is(10),
+                        "meta.count", is(1000),
+                        "data.size()", is(10),
+                        "data[0].ruc", is("20272209325")
+                );
+    }
+
+}
