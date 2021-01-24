d5a948868575ce3ddcbd993625672d5ceaf27657
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-24 20:22:49 +0100

    license fix

--- src/main/distribution/bin/standalone.bat
+++ src/main/distribution/bin/standalone.bat
@@ -1,3 +1,20 @@
+@REM
+@REM Copyright 2019 Project OpenUBL, Inc. and/or its affiliates
+@REM and other contributors as indicated by the @author tags.
+@REM
+@REM Licensed under the Eclipse Public License - v 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM https://www.eclipse.org/legal/epl-2.0/
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
 @echo off
 rem -------------------------------------------------------------------------
 rem JBoss Bootstrap Script for Windows
