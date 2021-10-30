49b7ec6bd191bbce6342e7effed56cd730b7a800
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-10-30 15:26:03 +0200

    upgrade version

--- src/main/java/io/github/project/openubl/searchpe/bootstrap/SearchpeBootstrap.java
+++ src/main/java/io/github/project/openubl/searchpe/bootstrap/SearchpeBootstrap.java
@@ -18,19 +18,36 @@ package io.github.project.openubl.searchpe.bootstrap;
 
 import io.github.project.openubl.searchpe.models.RoleType;
 import io.github.project.openubl.searchpe.models.jpa.entity.BasicUserEntity;
+import io.github.project.openubl.searchpe.models.jpa.entity.VersionEntity;
 import io.quarkus.runtime.StartupEvent;
+import org.hibernate.search.mapper.orm.session.SearchSession;
 
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.transaction.Transactional;
 
 @Singleton
 public class SearchpeBootstrap {
 
+    @Inject
+    SearchSession searchSession;
+
     @Transactional
     void createAdminUserOnStart(@Observes StartupEvent ev) {
         if (BasicUserEntity.count() == 0) {
             BasicUserEntity.add("admin", "admin", RoleType.admin.toString());
+        }
+    }
+
+    /**
+     * If there is an upgrade of Version, the indexes in Elasticsearch should be deleted.
+     * This will work under the assumption that on upgrades the DB will delete all data before.
+     */
+    @Transactional
+    void reindexSearchIndexes(@Observes StartupEvent ev) throws InterruptedException {
+        if (VersionEntity.count() == 0) {
+            searchSession.massIndexer().purgeAllOnStart(true).startAndWait();
         }
     }
 
--- src/main/resources/db/migration/V2.1.0__Upgrade.sql
+++ src/main/resources/db/migration/V2.1.0__Upgrade.sql
@@ -1,6 +1,12 @@
+-- Delete all preexisting SUNAT data
+delete from CONTRIBUYENTE;
+delete from VERSION;
+
+-- Alter tables to match new Models
 alter table if exists VERSION add column version int8 not null;
 alter table if exists CONTRIBUYENTE add column tipo_contribuyente varchar (15) not null;
 
+-- Create new table for Basic Auth support
 CREATE TABLE BASIC_USER
 (
     id       int8         NOT NULL,
