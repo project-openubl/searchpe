a6438bdea4e79debe98f7e5fe15d940b85442e36
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-11-12 20:25:05 +0100

    env with ui

--- src/main/java/io/github/project/openubl/searchpe/resources/FrontendResource.java
+++ src/main/java/io/github/project/openubl/searchpe/resources/FrontendResource.java
@@ -54,7 +54,7 @@ public class FrontendResource {
     @ConfigProperty(name = "quarkus.oidc.auth-server-url")
     Optional<String> oidcServerUrl;
 
-    @ConfigProperty(name = "quarkus.oidc.client-id")
+    @ConfigProperty(name = "quarkus.oidc.ui-client-id")
     Optional<String> oidcClientId;
 
     @PermitAll
@@ -84,6 +84,6 @@ public class FrontendResource {
         return keycloakJSON
                 .data("oidcRealm", realmName)
                 .data("oidcServerUrl", serverUrl)
-                .data("oidcResource", "searchpe-ui");
+                .data("oidcResource", oidcClientId.orElse(""));
     }
 }
