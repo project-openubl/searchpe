f31e529cf2366d163fd7c2f36f58e5aac86a2881
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2022-05-15 10:42:20 +0200

    Remove indexes

--- src/main/resources/db/migration/V4.0.0__Upgrade.sql
+++ src/main/resources/db/migration/V4.0.0__Upgrade.sql
@@ -23,6 +23,6 @@ alter table if exists CONTRIBUYENTE rename column numero_documento to ruc;
 alter table if exists CONTRIBUYENTE add column dni varchar (8) null;
 alter table if exists CONTRIBUYENTE drop tipo_persona;
 
-create index contribuyente_version_fkey on public.contribuyente (version_id);
+-- create index contribuyente_version_fkey on public.contribuyente (version_id);
 create index contribuyente_dni_index on public.contribuyente (version_id, dni);
-create index contribuyente_nombre_index on public.contribuyente (version_id, nombre);
+-- create index contribuyente_nombre_index on public.contribuyente (version_id, nombre);
